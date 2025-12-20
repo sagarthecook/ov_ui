5198e8407f91839bc00b68c1dc0eb56ce5376fe6
thecook060 <thecook060@gmail.com>
2025-12-20 10:29:36 +0530

    URL updated

--- src/app/contants/url.enum.ts
+++ src/app/contants/url.enum.ts
@@ -11,5 +11,6 @@ export enum URLConstants {
     USER_DETAILS_SAVE = '/v1/user_detail',
     GET_USER_ROLES = '/v1/roles/',
     GET_MENU_ITEMS = '/v1/menu/user',
-    GET_VOTER_DETAILS_LIST = '/v1/user_detail/findbyStatus?status=Pending&orderBy=created_date&order=desc'
+    GET_VOTER_DETAILS_LIST = '/v1/user_detail/findbyStatus?status=Pending&orderBy=created_date&order=desc',
+    GET_USER_DETAILS_BY_STATUS = '/v1/user_detail/findbyStatus'
 }
