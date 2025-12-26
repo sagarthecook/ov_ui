3b000a9bcfcafed6ac6a58f8541148f88b7c108f
thecook060 <thecook060@gmail.com>
2025-12-26 22:09:28 +0530

    Update : Election Creation

---
+++ src/app/create-election/create-election.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'create_election',
+  imports: [],
+  templateUrl: './create-election.html',
+  styleUrl: './create-election.scss',
+})
+export class CreateElection {
+
+}
