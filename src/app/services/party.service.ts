5a3b40b71d3217152228a75cc23f0fc63992eb9d
Aishwaryaghogardare11 <aishwaryaghogardare@gmail.com>
2025-12-30 18:16:40 +0530

    candidateform

--- src/app/services/party.service.ts
+++ src/app/services/party.service.ts
@@ -4,6 +4,7 @@ import {Party} from '../models/party.model';
 import {APIResponse} from '../models/ApiResponse';
 import {URLConstants} from '../contants/url.enum';
 import {Observable} from 'rxjs';
+import {DropdownModel} from '../models/dropdown.model';
 
 @Injectable({
     providedIn: 'root',
@@ -12,5 +13,9 @@ export class PartyService {
     constructor(private httpClient: HttpClient) {}
     public saveParty(partyData: Party): Observable<APIResponse<any>> {
         return this.httpClient.post<APIResponse<any>>(URLConstants.BASE_URL + URLConstants.SAVE_PARTY, partyData );
+    }
+    public getParty(): Observable<APIResponse<DropdownModel[]>> {
+            return this.httpClient.get<APIResponse<DropdownModel[]>>(URLConstants.BASE_URL + URLConstants.GET_PARTY);
+            
     }
 }
