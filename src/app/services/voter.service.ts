867ebb37d9dfdf6678e9c3f894320141f314a776
thecook060 <thecook060@gmail.com>
2026-01-02 18:12:23 +0530

    Voter Approved screen

--- src/app/services/voter.service.ts
+++ src/app/services/voter.service.ts
@@ -3,20 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { APIResponse } from '../models/ApiResponse';
 import { URLConstants } from '../contants/url.enum';
 import { Observable } from 'rxjs';
-import { Voters } from '../voter-search/votersearch';
+import { Voter } from '../models/voter.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VoterService {
-
   constructor(private httpClient: HttpClient) {}
 
-getVoters(phone?: string, email?: string): Observable<Voters[]> {
-  let params = [];
-  if (phone) params.push(`phone=${encodeURIComponent(phone)}`);
-  if (email) params.push(`email=${encodeURIComponent(email)}`);
-  const url = URLConstants.BASE_URL + URLConstants.VOTER_SEARCH + params.join('&');
-  return this.httpClient.get<Voters[]>(url);
-}
+  searchVoter(searchOption:string , searchValue : string): Observable<APIResponse<Voter[]>> {
+   return this.httpClient.get<APIResponse<Voter[]>>(
+      URLConstants.BASE_URL +`${URLConstants.VOTER_SEARCH}?${searchOption}=${searchValue}`
+    );
+  }
 }
--- src/app/voter-search/votersearch.ts
+++ src/app/voter-search/votersearch.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subject, Subscription } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { VoterService } from '../services/voter.service';
 import { MatCardHeader, MatCardTitle, MatCard, MatCardModule } from "@angular/material/card";
 import { CommonModule } from '@angular/common';
@@ -14,21 +13,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatPaginator } from "@angular/material/paginator";
 
 
-export interface Voters {
-  electionId: number;
-  fullName: string;
-  aadhaar: string;
-  phone: string;
-  email: string;
-  status: string;
-}
-
 @Component({
-  selector: 'app-votersearch',
+  selector: 'votersearch',
   imports: [MatCardHeader, MatCardTitle,
     CommonModule,
     ReactiveFormsModule,
@@ -49,34 +39,44 @@ export interface Voters {
 
 
 export class VoterSearchComponent implements OnInit, OnDestroy {
-applyFilter($event: KeyboardEvent) {
-throw new Error('Method not implemented.');
-}
   searchInput$ = new Subject<string>();
   searchSub!: Subscription;
   dataSource: any[] = [];
+  selectedSearchOption: string = 'email';
+  searchValue: string = '';
   displayedColumns: string[] = [
-    'electionId', 'fullName', 'aadhaar', 'phone', 'email', 'status'
+    'id', 'firstName', 'lastName', 'emailId', 'phoneNumber', 'dateOfBirth', 'aadharNumber', 'status'
   ];
 
   constructor(private voterService: VoterService) {}
 
   ngOnInit() {
-    this.searchSub = this.searchInput$
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged()
-      )
-      .subscribe(searchValue => {
-        // You can split searchValue if you want to support both phone and email
-        this.voterService.getVoters(searchValue, searchValue).subscribe(results => {
-          this.dataSource = results;
-        });
-      });
   }
 
-  onSearchInput(value: string) {
-    this.searchInput$.next(value);
+  applyFilter(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.searchValue = value;
+  }
+
+  onSearchOptionChange(event: any) {
+    this.selectedSearchOption = event.value;
+    this.searchValue='';
+    this.dataSource = [];
+    console.log('Search option changed to:', event.value);
+  }
+
+  onSearchClick() {
+    this.voterService.searchVoter(this.selectedSearchOption, this.searchValue).subscribe(response => {
+      if (response && response.data) {
+        this.dataSource = response.data;
+      } else {
+        this.dataSource = [];
+      }
+    },(error) => {
+      console.error('Error fetching voter data:', error);
+      this.dataSource = [];
+    });
   }
 
   ngOnDestroy() {
